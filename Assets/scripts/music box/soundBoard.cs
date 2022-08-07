5cfae532984afcab32c324fb2993f5efc11c9dcf
asala00 <asalastrying@gmail.com>
2022-08-07 14:19:14 +0300

    added audio input controls

---
+++ Assets/scripts/music box/soundBoard.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundBoard : MonoBehaviour
+{
+    private AudioSource _audioSource; //the Audio Source component itself so we can control it thro script
+    void Start()
+    {
+       // Using the Start method to directly grab the Audio Source component from the game object itself and plug it in as our variable.
+       _audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        MusicBoxControls();
+    }
+
+    void MusicBoxControls()  //will code the audio to start acoording to input instead of on awake
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            _audioSource.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _audioSource.Stop();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            _audioSource.Pause();
+        }
+    }
+}
